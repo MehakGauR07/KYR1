077d30c49d676e7a94fda89e9dfc2174fb953d60
AkankshRakesh <akankshrakesh@gmail.com>
2024-09-08 15:20:55 +0530

    arseHOLE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -60,32 +60,32 @@ app.get('/api/options/case2/:article', async (req: Request, res: Response) => {
 // const VERIFY_TOKEN = "youaregay";
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
+// app.get('/webhook', (req, res) => {
   
-  // Your verify token. Should be a string you set.
-  let VERIFY_TOKEN = "youaregay";
+//   // Your verify token. Should be a string you set.
+//   let VERIFY_TOKEN = "youaregay";
   
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+//   // Parse the query params
+//   let mode = req.query['hub.mode'];
+//   let token = req.query['hub.verify_token'];
+//   let challenge = req.query['hub.challenge'];
   
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
+//   // Checks if a token and mode is in the query string of the request
+//   if (mode && token) {
     
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+//     // Checks the mode and token sent is correct
+//     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+//       // Responds with the challenge token from the request
+//       console.log('WEBHOOK_VERIFIED');
+//       res.status(200).send(challenge);
       
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
+//     } else {
+//       // Responds with '403 Forbidden' if verify tokens do not match
+//       res.sendStatus(403);      
+//     }
+//   }
+// });
 
 // ----------------------------------------------
 
